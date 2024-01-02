9db34e530c5d7fd673314c8ce8e335f552bd1d57
jagannath1234 <jagannathjena47@gmail.com>
2024-01-02 11:06:31 +0530

    report

--- student_reg/src/app/services/auth/role.guard.ts
+++ student_reg/src/app/services/auth/role.guard.ts
@@ -1,7 +1,7 @@
 import { CanActivateFn } from '@angular/router';
 
 export const roleGuard: CanActivateFn = (route, state) => {
-  // console.log(route,state.url)
+  console.log(state.url)
   let role:any = sessionStorage.getItem('userRole')?.toLowerCase()
   if(state.url.includes(role)){
     return true
